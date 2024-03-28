b79657177851e09a7764f0364dc0ce028671bfd5
William Moore <w.moore@dundee.ac.uk>
2024-03-28 13:20:28 +0000

    Add omezarr.alt_store option to .bfoptions

--- src/omero_mkngff/__init__.py
+++ src/omero_mkngff/__init__.py
@@ -260,19 +260,19 @@ class MkngffControl(BaseControl):
         if args.symlink_repo:
             self.create_symlink(args.symlink_repo, prefix, args.symlink_target)
             if args.bfoptions:
-                self.write_bfoptions(args.symlink_repo, prefix, args.symlink_target)
+                self.write_bfoptions(args.symlink_repo, prefix, args.symlink_target, args.clientpath)
 
     def bfoptions(self, args: Namespace) -> None:
         self.suffix = "" if args.fs_suffix == "None" else args.fs_suffix
         prefix = self.get_prefix(args)
-        self.write_bfoptions(args.symlink_repo, prefix, args.symlink_target)
+        self.write_bfoptions(args.symlink_repo, prefix, args.symlink_target, args.clientpath)
 
     def symlink(self, args: Namespace) -> None:
         self.suffix = "" if args.fs_suffix == "None" else args.fs_suffix
         prefix = self.get_prefix(args)
         self.create_symlink(args.symlink_repo, prefix, args.symlink_target)
         if args.bfoptions:
-            self.write_bfoptions(args.symlink_repo, prefix, args.symlink_target)
+            self.write_bfoptions(args.symlink_repo, prefix, args.symlink_target, args.clientpath)
 
     def get_prefix(self, args):
 
@@ -304,16 +304,19 @@ class MkngffControl(BaseControl):
         symlink_dir = f"{prefix_dir}{self.suffix}"
         return symlink_dir
 
-    def write_bfoptions(self, managed_repo, fsprefix, symlink_target):
+    def write_bfoptions(self, managed_repo, fsprefix, symlink_target, clientpath=None):
         file_path = Path(symlink_target)
         mkngff_dir = self.get_symlink_dir(managed_repo, fsprefix)
         # os.makedirs(mkngff_dir, exist_ok=True)
         zarr_path = os.path.join(mkngff_dir, file_path.name)
         bfoptions_path = f"{zarr_path}.bfoptions"
         self.ctx.err("write bfoptions to: %s" % bfoptions_path)
+        lines = ["omezarr.list_pixels=false\n",
+                 "omezarr.quick_read=true\n"]
+        if clientpath is not None:
+            lines.append("omezarr.alt_store={clientpath}\n")
         with open(bfoptions_path, "w") as f:
-            f.writelines(["omezarr.list_pixels=false",
-                          "\nomezarr.quick_read=true"])
+            f.writelines(lines)
 
     def create_symlink(self, symlink_repo, prefix, symlink_target):
         symlink_path = Path(symlink_target)
